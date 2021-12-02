a028e658d1327e4cf18e6ee76103b61fdb35fe15
Maksym Skorbatiuk <57547446+MakenziGilbert@users.noreply.github.com>
2021-12-02 18:48:05 +0200

    add first two class

--- SPZ-2/Program.cs
+++ SPZ-2/Program.cs
@@ -6,11 +6,74 @@ using System.Windows.Forms;
 
 namespace SPZ_2
 {
+    public class Port
+    {
+        string name;
+        string adress;
+        Pirce nPirce;
+        double costTechnique = 10;
+        double costService = 15;
+        double timeService = 60; // время обслуживание в минутах
+
+
+        public Port()
+        {
+            name = "Name port";
+            adress = "Adress port";
+        }
+
+        public Port(string _name, string _adress, long _nPirce, long _nWorkers)
+        {
+            name = _name;
+            adress = _adress;
+            nPirce.setPirce(_nPirce);
+
+        }
+    }
+
+    class Pirce : Port
+    {
+        long nPirce;
+        long nWorkers;
+        long nTechnique;
+
+        public Pirce()
+        {
+            nPirce = 1;
+            nTechnique = 5;
+        }
+
+        public Pirce(long _nPirce)
+        {
+            nPirce = _nPirce;
+            nTechnique = nPirce * 5;
+        }
+
+        public void setPirce(long _nPirce)
+        {
+            nPirce = _nPirce;
+            nTechnique = nPirce * 15;
+        }
+
+        public void setWorkers(long _nWorkers)
+        {
+            if (nPirce > 0)
+                if (_nWorkers / nPirce <= 15)
+                {
+                    nWorkers = _nWorkers;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка! Рабочих больше чем рабочиз мест!");
+                }
+            else
+                MessageBox.Show("Ошибка! Нету пирсов.");
+        }
+    }
+
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
